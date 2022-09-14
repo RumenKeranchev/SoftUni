6d61fcfc4fe3c4bc52d9a0905428505b675d8f17
Rumen Keranchev <devtestmail69@gmail.com>
2022-09-14 21:39:34 +0300

    Html And CSS Homework No:1

---
+++ 05 - Frontend/HTML+CSS/01-Intro to HTML and CSS/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _01_Intro_to_HTML_and_CSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/welcome.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Fruits()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/fruits.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Wiki()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/wiki-page.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult ToDo()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/to-do-list.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Lists()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/html-lists.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Dls()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/definition-lists.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Rl()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/reversed-list.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Js()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/modern-javascript.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Book()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/book-story.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult News()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/world-cup-news.html"),
+                ContentType = "text/html"
+            };
+        }
+    }
+}
---
+++ 05 - Frontend/HTML+CSS/01-Intro to HTML and CSS/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddMvc();
+var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Welcome}");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Views\\Home")),
+    RequestPath = "/Home"
+});
+
+app.Run();
