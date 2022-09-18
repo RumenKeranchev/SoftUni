916a0294a788b08bda6fb5acaee70c4ba3521866
Rumen Keranchev <devtestmail69@gmail.com>
2022-09-18 15:05:39 +0300

    Html and CSS Homework No:2

---
+++ 05 - Frontend/HTML+CSS/02-HTML Structure/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _02_HTML_Structure.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/Home.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Nav()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/nav-bar.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Page()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/page-content.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Tags()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/semantic-tags.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Article()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/semantic-article.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Website()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/simple-website.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Blog()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/index.html"),
+                ContentType = "text/html"
+            };
+        }
+    }
+}
