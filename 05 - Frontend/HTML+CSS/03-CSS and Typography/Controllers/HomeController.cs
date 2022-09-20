99813f75ae05c9ca8956c4e998ee082bf2248786
Rumen Keranchev <devtestmail69@gmail.com>
2022-09-20 20:28:28 +0300

    Html and CSS Homework No:2 pt1

---
+++ 05 - Frontend/HTML+CSS/03-CSS and Typography/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _03_CSS_and_Typography.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/Home.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Menu()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/restaurant-menu.html"),
+                ContentType = "text/html"
+            };
+        }
+
+        public IActionResult Lists()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/style-lists.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Tables()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/styling-tables.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Buttons()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/index.html"),
+                ContentType = "text/html"
+            };
+        }
+    }
+}
