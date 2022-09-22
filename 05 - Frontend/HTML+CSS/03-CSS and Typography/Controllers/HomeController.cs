1415469940c35ac906550c2d1c2bb1d40a8fe66b
Rumen Keranchev <devtestmail69@gmail.com>
2022-09-22 18:10:20 +0300

    Html and CSS Homework No:3 pt2

--- 05 - Frontend/HTML+CSS/03-CSS and Typography/Controllers/HomeController.cs
+++ 05 - Frontend/HTML+CSS/03-CSS and Typography/Controllers/HomeController.cs
@@ -44,7 +44,70 @@ namespace _03_CSS_and_Typography.Controllers
         {
             return new ContentResult
             {
-                Content = System.IO.File.ReadAllText("./Views/Home/index.html"),
+                Content = System.IO.File.ReadAllText("./Views/Home/buttons-css.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Speciment()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/fonts-speciment.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Colors()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/contrasting-colors.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Font()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/making-css-font.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult FontIcons()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/font-awesome-icons.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult ButtonIcons()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/icon-font-buttons.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult ListIcons()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/icon-font-list.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Typography()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/typography.html"),
                 ContentType = "text/html"
             };
         }
