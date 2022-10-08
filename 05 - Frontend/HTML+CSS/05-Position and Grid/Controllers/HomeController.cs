a677799ca1128b098e991cc01e577b1f1705bd62
Rumen Keranchev <devtestmail69@gmail.com>
2022-10-08 15:15:25 +0300

    Html and CSS Homework No:5 initial. Renamed last homework project for consistency

---
+++ 05 - Frontend/HTML+CSS/05-Position and Grid/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _04_CSS_Box_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/Home.html"),
+                ContentType = "text/html"
+            };
+        }
+    }
+}
