5f2a10c1b20106a47b7d38d217938502b2928d82
Rumen Keranchev <devtestmail69@gmail.com>
2022-10-23 12:20:31 +0300

    Homework 07 initial

---
+++ 05 - Frontend/HTML+CSS/07-Media Queries/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _04_CSS_Box_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/Home.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult SimpleLayout()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/simple-site-layout.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult Navigation()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/navigation.html"),
+                ContentType = "text/html"
+            };
+        }
+        
+        public IActionResult ResponsiveForms()
+        {
+            return new ContentResult
+            {
+                Content = System.IO.File.ReadAllText("./Views/Home/responsive-forms.html"),
+                ContentType = "text/html"
+            };
+        }
+    }
+}
